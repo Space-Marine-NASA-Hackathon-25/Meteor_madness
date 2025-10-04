1e05e0d6143bf4f7a8bdcf86346c5f89abd69c07
Lians-coder <rexdfhr@gmail.com>
2025-10-04 21:44:12 +0300

    add kernels

---
+++ services/api_calls.py
@@ -0,0 +1,24 @@
+FIELDS = [
+    "full_name", "H", "G", "density", "albedo",
+    "e", "a", "q", "i", "om", "w", "ma", "tp", "per", "n", "ad", "moid_ld"]
+
+
+def call_sentry(designation):
+    pass
+
+
+def call_horizons():
+    pass
+
+
+def call_smdb():
+    pass
+
+
+def convert_horizons2spice():
+    pass
+
+
+def convert_smdb2spice():
+    pass
+
--- services/services.py
+++ services/services.py
@@ -1,1 +1,15 @@
+from pathlib import Path
 from services.api_calls import *
+
+DIR_KERNELS = "kernels"
+KERNELS_PATHS = ["spk/de432s.bsp", "lsk/naif0012.tls", "pck/gm_de440.tpc"]
+
+def get_paths_to_kernels(dir_kernels=DIR_KERNELS, paths=KERNELS_PATHS):
+    """ Returns absolute path for each kernel file """
+    # https://naif.jpl.nasa.gov/pub/naif/generic_kernels/
+    BASE = Path(__file__).resolve().parent
+    KERNELS = BASE / dir_kernels
+    paths_to_kernels = []
+    for p in paths:
+        paths_to_kernels.append(str(KERNELS / p))
+    return paths_to_kernels
