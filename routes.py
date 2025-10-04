c1ef036c82ad050a236ae9067bfa835d305277c1
Lians-coder <rexdfhr@gmail.com>
2025-10-04 20:04:11 +0300

    add templates & routes

--- routes.py
+++ routes.py
@@ -14,8 +14,7 @@ def search_asteroid_daesignation():
     """ Search APIs for info + parameters? POST return text data """
     if request.method == "POST":
         pass
-    else:
-        return render_template("search.html")
+    return render_template("search.html")
     
     
 @bp.route("/result")
@@ -23,11 +22,21 @@ def result_page(threat=False):
     """ Provide current most-like trajectory for PHA """
     if threat:
         return render_template("result.html")
-    else:
-        render_template("no_threat.html")
+    return render_template("no_threat.html")
 
 
 @bp.route("/simulation")
 def simulation():
     """ Run simulation of hitting Earth """
     return render_template("simulation.html")
+
+
+@bp.route("/mitigation", methods=["GET", "POST"])
+def mitigation():
+    """ Choose method of mitigation PHA """
+    if request.method == "POST":
+        name = request.form.get("name")
+        if name not in ["general", "deflection", "tractor", "nuclear"]:
+            return render_template("apology.html")
+        return render_template(f"{name}.html")
+    return render_template("mitigation.html")
