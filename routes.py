6c1e0b52b436b7d78b56a67f4b7e9a309f8a11c7
Lians-coder <rexdfhr@gmail.com>
2025-10-04 11:05:55 +0300

    add routes

--- routes.py
+++ routes.py
@@ -1,8 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from services import *
 
 bp = Blueprint("routes", __name__, url_prefix="")
 
 @bp.route("/")
 def main_page():
+    """ Base route """
     return render_template("index.html")
+
+
+@bp.route("/search", methods=["GET", "POST"])
+def search_asteroid_daesignation():
+    """ Search APIs for info + parameters? POST return text data """
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("search.html")
+    
+    
+@bp.route("/result")
+def result_page():
+    """ Provide current most-like trajectory for PHA """
+    return render_template("result.html")
+
+
+@bp.route("/simulation")
+def simulation():
+    """ Run simulation of hitting Earth """
+    return render_template("simulation.html")
